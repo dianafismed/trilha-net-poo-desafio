5271ca308c6eeb6217b35a78a2b021492fec4f36
Diana Avila <missernightangel@gmail.com>
2025-09-15 18:42:53 -0300

    up

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nIphone");
+
+Iphone iphone = new Iphone("987654321", "iPhone 13", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
