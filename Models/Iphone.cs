fe70da9f43af8a44055bbbe82fde26fcade36c4c
Diana Avila <missernightangel@gmail.com>
2025-09-15 18:36:24 -0300

    up

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,10 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
-    
 
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Implementação do método abstrato
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nome} no Iphone");
+        }
     }
 }
